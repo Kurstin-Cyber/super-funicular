61e6e82e64ddfeeb45eb0f6b1d7916e1a0bb19f4
Kurstin-Cyber <54408594+Kurstin-Cyber@users.noreply.github.com>
2022-06-18 01:39:32 -0400

    add

---
+++ test_excercise_5.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+def test_addition_1():
+    assert addition(6, 7) == 13
+
+
+def test_addition_2():
+    assert addition(100, 30) == 130
+
+
+def test_addition_3():
+    assert addition(10, 75) == 85
